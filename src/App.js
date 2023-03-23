ae020cc2d658e27f1ec4552f44b1113ce438df82
Abewd <116496332+abewd@users.noreply.github.com>
2023-03-23 21:53:11 +1100

    adjusted text alignment, its nice now

--- src/App.js
+++ src/App.js
@@ -38,10 +38,12 @@ const App = () => {
           <div key={user.id} className="User-item">
             <h2>{user.name}</h2>
             <h5>&#9993; {user.email}</h5>
-            <h5>
-              📍 {user.address.suite}, {user.address.street},{" "}
-              {user.address.city}, {user.address.zipcode}
-            </h5>
+            <div className="addressEdit">
+              <h5>
+                📍{user.address.suite}, {user.address.street},{" "}
+                {user.address.city}, {user.address.zipcode}
+              </h5>
+            </div>
             <h5>📞 {user.phone}</h5>
             <h5>🌐 {user.website}</h5>
             <h5>🏭 {user.company.name}</h5>
